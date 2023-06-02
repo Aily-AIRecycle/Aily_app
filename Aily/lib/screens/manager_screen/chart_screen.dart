86cb92db307c8ce1a060aaf1bc8b090f41217c2f
yoonchan <tlsdbscks123@gmail.com>
2023-06-02 16:53:53 +0900

    프로젝트 버전 변경

---
+++ Aily/lib/screens/manager_screen/chart_screen.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:gradients/gradients.dart';
+import 'dart:math';
+
+class ChartScreen extends StatefulWidget {
+  const ChartScreen({Key? key}) : super(key: key);
+
+  @override
+  ChartScreenState createState() => ChartScreenState();
+}
+
+class ChartScreenState extends State<ChartScreen> {
+  Color myColor = const Color(0xFFF8B195);
+  Color backColor = const Color(0xFFF6F1F6);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backColor,
+      body: SingleChildScrollView(
+        child: ChartWidget(context),
+      ),
+    );
+  }
+
+  Widget ChartWidget(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(
+              top: 72, left: 24, right: 24, bottom: 24),
+          child: Column(
+            children: [
+
+              Container(
+                height: 0.5,
+                width: MediaQuery.of(context).size.width * 0.85,
+                color: Colors.grey,
+              ),
+              const SizedBox(height: 20),
+              const Text('월별', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+              //SizedBox(height: MediaQuery.of(context).size.height * 0.5),
+              CustomPaint(
+                size: Size(MediaQuery.of(context).size.width * 0.85, MediaQuery.of(context).size.height * 0.4),
+                foregroundPainter: BarChart(
+                  data: <double>[105, 55, 99, 150, 300, 500, 120, 1000, 1300, 1800],
+                  labels: <String>[
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7',
+                    '8',
+                    '9',
+                    '10'
+                  ],
+                  color: Colors.deepOrange,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BarChart extends CustomPainter {
+  final Color color;
+  final List<double> data;
+  final List<String> labels;
+  double bottomPadding = 0.0;
+  double leftPadding = 0.0;
+  double textScaleFactorXAxis = 0.5; //X 폰트사이즈
+  double textScaleFactorYAxis = 1.2; //Y 폰트사이즈
+
+  BarChart({required this.data, required this.labels, this.color = Colors.blue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // 텍스트 공간을 미리 정한다.
+    setTextPadding(size);
+
+    List<Offset> coordinates = getCoordinates(size);
+
+    drawBar(canvas, size, coordinates);
+    drawXLabels(canvas, size, coordinates);
+    drawYLabels(canvas, size, coordinates);
+    drawLines(canvas, size, coordinates);
+  }
+
+  @override
+  bool shouldRepaint(BarChart oldDelegate) {
+    return oldDelegate.data != data;
+  }
+
+  void setTextPadding(Size size) {
+    // 세로 크기의 1/10만큼 텍스트 패딩
+    bottomPadding = size.height / 10;
+    // 가로 길이 1/10만큼 텍스트 패딩
+    leftPadding = size.width / 10;
+  }
+
+  void drawBar(Canvas canvas, Size size, List<Offset> coordinates) {
+    Paint paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    // 막대 그래프 넓이
+    double barWidthMargin = size.width * 0.05;
+    double barOffset = 7.0;
+
+    for (int index = 0; index < coordinates.length; index++) {
+      Offset offset = coordinates[index];
+      double left = offset.dx + barOffset;
+      // 간격만큼 가로로 이동
+      double right = offset.dx + barOffset + barWidthMargin;
+
+      double top = offset.dy;
+      // 텍스트 크기만큼 패딩을 빼준다. 그래서 텍스트와 겹치지 않게 한다.
+      double bottom = size.height - bottomPadding;
+
+      Rect rect = Rect.fromLTRB(left, top, right, bottom);
+      canvas.drawRect(rect, paint);
+
+      // 데이터 값을 그래프 위에 표시
+      String dataValue = '${data[index].toInt()}';
+      TextSpan span = TextSpan(
+        style: TextStyle(
+          color: Colors.blueAccent.shade700,
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+        ),
+        text: dataValue,
+      );
+      TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+      tp.layout();
+
+      double textX = left + (barWidthMargin / 2) - (tp.width / 2);
+      double textY = top - tp.height - 4; // 4는 데이터 값과 막대 사이의 여백
+
+      tp.paint(canvas, Offset(textX, textY));
+    }
+  }
+
+
+  // x축 텍스트(레이블)를 그린다.
+  void drawXLabels(Canvas canvas, Size size, List<Offset> coordinates) {
+    // 화면 크기에 따라 유동적으로 폰트 크기를 계산한다.
+    double fontSize = calculateFontSize(labels[0], size, xAxis: true);
+
+    for (int index = 0; index < labels.length; index++) {
+      TextSpan span = TextSpan(
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: fontSize,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.w400,
+        ),
+        text: labels[index],
+      );
+      TextPainter tp =
+      TextPainter(text: span, textDirection: TextDirection.ltr);
+      tp.layout();
+
+      Offset offset = coordinates[index];
+      double dx = offset.dx;
+      double dy = size.height - tp.height;
+
+      tp.paint(canvas, Offset(dx, dy));
+    }
+  }
+
+  // Y축 텍스트(레이블)를 그린다. 최저값과 최고값을 Y축에 표시한다.
+  void drawYLabels(Canvas canvas, Size size, List<Offset> coordinates) {
+    double bottomY = coordinates[0].dy;
+    double topY = coordinates[0].dy;
+    int indexOfMin = 0;
+    int indexOfMax = 0;
+
+    for (int index = 0; index < coordinates.length; index++) {
+      double dy = coordinates[index].dy;
+      if (bottomY < dy) {
+        bottomY = dy;
+        indexOfMin = index;
+      }
+      if (topY > dy) {
+        topY = dy;
+        indexOfMax = index;
+      }
+    }
+    String minValue = '${data[indexOfMin].toInt()}';
+    String maxValue = '${data[indexOfMax].toInt()}';
+
+    double fontSize = calculateFontSize(maxValue, size, xAxis: false);
+
+    drawYText(canvas, minValue, fontSize, bottomY);
+    drawYText(canvas, maxValue, fontSize, topY);
+  }
+
+  // 화면 크기에 비례해 폰트 크기를 계산한다.
+  double calculateFontSize(String value, Size size, {required bool xAxis}) {
+    // 글자수에 따라 폰트 크기를 계산하기 위함
+    int numberOfCharacters = value.length;
+    // width가 600일 때 100글자를 적어야 한다면, fontSize는 글자 하나당 6이어야 한다.
+    double fontSize = (size.width / numberOfCharacters) / data.length;
+
+    if (xAxis) {
+      fontSize *= textScaleFactorXAxis;
+    } else {
+      fontSize *= textScaleFactorYAxis;
+    }
+    return fontSize;
+  }
+
+  // x축 & y축 구분하는 선을 그린다.
+  void drawLines(Canvas canvas, Size size, List<Offset> coordinates) {
+    Paint paint = Paint()
+      ..color = Colors.blueGrey.shade100
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.8;
+
+    double bottom = size.height - bottomPadding;
+    double left = coordinates[0].dx;
+
+    Path path = Path();
+    path.moveTo(left, 0);
+    path.lineTo(left, bottom);
+    path.lineTo(size.width, bottom);
+
+    canvas.drawPath(path, paint);
+  }
+
+  void drawYText(Canvas canvas, String text, double fontSize, double y) {
+    TextSpan span = TextSpan(
+      style: TextStyle(
+        fontSize: fontSize,
+        color: Colors.black,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w600,
+      ),
+      text: text,
+    );
+    TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+
+    tp.layout();
+
+    Offset offset = Offset(0.0, y);
+    tp.paint(canvas, offset);
+  }
+
+  List<Offset> getCoordinates(Size size) {
+    List<Offset> coordinates = <Offset>[];
+
+    double maxData = data.reduce(max);
+
+    double width = size.width - leftPadding;
+    double minBarWidth = width / data.length;
+
+    for (int index = 0; index < data.length; index++) {
+      // 그래프의 가로 위치를 정한다.
+      double left = minBarWidth * (index) + leftPadding;
+      // 그래프의 높이가 [0~1] 사이가 되도록 정규화 한다.
+      double normalized = data[index] / maxData;
+      // x축에 표시되는 글자들과 겹치지 않게 높이에서 패딩을 제외한다.
+      double height = size.height - bottomPadding;
+      // 정규화된 값을 통해 높이를 구한다.
+      double top = height - normalized * height;
+
+      Offset offset = Offset(left, top);
+      coordinates.add(offset);
+    }
+
+    return coordinates;
+  }
+}
---
+++ Aily/lib/screens/map_screen/map_screen.dart
@@ -0,0 +1,415 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:aily/screens/map_screen/garbage_screen.dart';
+import 'package:aily/utils/ShowDialog.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import '../../class/URLs.dart';
+import '../../class/UserData.dart';
+import '../../class/location.dart';
+import '../../class/garbageData.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  bool get wantKeepAlive => true;
+  Color myColor = const Color(0xFFF8B195);
+  late TextEditingController searchctrl;
+  late String searchStr = '';
+  final FocusNode _focusNode = FocusNode();
+  late final WebViewController _controller;
+  late String label, distance = '';
+  List<List<String>> resultList = [];
+  Location location = Location();
+  GarbageMerch merch = GarbageMerch();
+  UserData user = UserData();
+  bool updatebool = false;
+  bool status = false;
+  late int gen = 0;
+  late int can = 0;
+  late int pet = 0;
+  Timer? timer, timer2;
+  late double latitude = 0.0;
+  late double longitude = 0.0;
+  late StreamSubscription<Position> positionStream;
+  final LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+    WebViewController.fromPlatformCreationParams(params);
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith(URL().mapURL)) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'onClickMarker',
+        onMessageReceived: (JavaScriptMessage message) {
+          final String msg = message.message;
+          List<String> messages = msg.split('\n');
+          for (var message in messages) {
+            List<String> parts = message.split(':');
+            String label = parts[0];
+            String distance = parts[1];
+            if (label.isNotEmpty && distance.isNotEmpty) {
+              Location().data[label] = distance;
+            }
+          }
+        },
+      )
+      ..loadRequest(Uri.parse(URL().mapURL));
+
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+
+    _controller = controller;
+
+    searchctrl = TextEditingController();
+    _getLocation();
+    _getDistance();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) => _getDistance());
+  }
+
+  void _getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      latitude = position.latitude;
+      longitude = position.longitude;
+    });
+  }
+
+  @override
+  void dispose() {
+    searchctrl.dispose();
+    timer?.cancel();
+    timer2?.cancel();
+    super.dispose();
+  }
+
+  String _getSearchString(String inputStr, List<GarbageData> garbageData) {
+    String searchStr = '';
+    status = garbageData[0].status;
+    gen = garbageData[0].gen;
+    can = garbageData[0].can;
+    pet = garbageData[0].pet;
+
+    if (inputStr.contains('Aily1') || inputStr.contains('고척')) {
+      searchStr = '고척스카이돔';
+    } else if (inputStr.contains('Aily2') || inputStr.contains('3호')) {
+      searchStr = '3호관';
+    } else if (inputStr.isEmpty) {
+      searchStr = '';
+    }
+    return searchStr;
+  }
+
+  Future<void> _updateDistanceText() async {
+    final String str = searchctrl.text.trim();
+    List<GarbageData> garbageData = await fetchGarbage(str);
+    merch.merch = str;
+    final String searchStr = _getSearchString(str, garbageData);
+    distance = Location().data[searchStr]!;
+    setState(() {});
+  }
+
+  void _search() {
+    final String str = searchctrl.text.trim();
+    if (str.contains('Aily1') || str.contains('동양')) {
+      searchStr = '고척스카이돔';
+      distance = Location().data[searchStr]!;
+    } else if (str.contains('Aily2') || str.contains('3호')) {
+      searchStr = '3호관';
+      distance = Location().data[searchStr]!;
+    } else if (str.isEmpty) {
+      showMsg(context, '검색', '검색어를 입력해주세요.');
+      searchStr = '';
+    } else {
+      showMsg(context, '검색', '찾을 수 없습니다.');
+    }
+    _removeFocus();
+    updatebool = true;
+    setState(() {});
+  }
+
+  void _removeFocus() {
+    _focusNode.unfocus();
+  }
+
+  void _getDistance() async {
+    //내 위치를 실시간으로 보냄
+    if (latitude != 0.0){
+      if (user.nickname != "관리자"){
+        await _controller.runJavaScript("getDistance($latitude, $longitude, false)");
+      }else{
+        await _controller.runJavaScript("getDistance($latitude, $longitude, true)");
+      }
+      if (updatebool && !_focusNode.hasFocus) {
+        _updateDistanceText();
+      } else {
+        updatebool = false;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: MapWidget(context),
+    );
+  }
+
+  Widget _buildListTiles() {
+    List<Widget> listTiles = [];
+    if (searchStr.isNotEmpty) {
+      listTiles.add(_ListTile(context, searchStr, int.parse(distance), status, gen, can, pet));
+    }
+    return Column(children: listTiles);
+  }
+
+  Widget MapWidget(BuildContext context) {
+    double ratio = MediaQuery.of(context).devicePixelRatio * 0.4;
+    return GestureDetector(
+      onTap: () {
+        _focusNode.unfocus();
+      },
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Expanded(
+              child: ClipRect(
+                child: Transform.scale(
+                  scale: ratio,
+                  child: WebViewWidget(controller: _controller),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      Column(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width - 48,
+                            height: 50,
+                            child: TextField(
+                              textInputAction: TextInputAction.search,
+                              onSubmitted: (value) {
+                                _search();
+                              },
+                              style: TextStyle(color: Colors.grey.shade600),
+                              focusNode: _focusNode,
+                              controller: searchctrl,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                hintText: '주소, 지역 검색',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: myColor),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                  BorderSide(color: Colors.grey.shade400),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                suffixIcon: IconButton(
+                                  color: Colors.grey.shade400,
+                                  icon: const Icon(Icons.search),
+                                  onPressed: () {
+                                    _search();
+                                  },
+                                ),
+                              ),
+                              obscureText: false,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Column(
+                            children: [
+                              const Text('현 위치에서 가까운 Aily의 위치가 나타나요.'),
+                              const SizedBox(height: 20),
+                              _buildListTiles(),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _ListTile(BuildContext context, String title, int distance, bool isAvailable, int gen, can, pet) {
+  Color myColor = const Color(0xFFF8B195);
+  GarbageMerch merch = GarbageMerch();
+
+  return Card(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(color: myColor.withOpacity(0.25)),
+      borderRadius: BorderRadius.circular(15.0),
+    ),
+    elevation: 0.25,
+
+    margin: const EdgeInsets.symmetric(horizontal: 35),
+    child: Theme(
+      data: ThemeData().copyWith(
+          dividerColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent),
+      child: ListTile(
+        horizontalTitleGap: 0,
+        contentPadding:
+        const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+        leading: Icon(Icons.directions_walk, color: myColor),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            color: myColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "남은 거리",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xff989898),
+                  ),
+                ),
+                Text('${distance}M\n'),
+              ],
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  '상태',
+                  style: TextStyle(fontSize: 12, color: Color(0xff989898)),
+                ),
+                const SizedBox(height: 5),
+                isAvailable
+                    ? const Icon(Icons.circle,
+                    color: Colors.lightGreenAccent, size: 14)
+                    : const Icon(Icons.circle, color: Colors.red, size: 14),
+                const SizedBox(height: 3),
+                isAvailable
+                    ? const Text(
+                  '사용 가능',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xff767676),
+                  ),
+                )
+                    : const Text(
+                  '사용 불가',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xff767676),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        onTap: () {
+          UserData user = UserData();
+          if (user.nickname == "관리자"){
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => GarbageScreen(title: merch.merch!, gen: gen, can: can, pet: pet)));
+          }
+        },
+      ),
+    ),
+  );
+}
